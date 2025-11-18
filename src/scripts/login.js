822035a59d438fb429568bcec0352af9f6898aa8
alexgeracaotech <alexbessa.dev@gmail.com>
2025-11-18 14:32:32 -0300

    finish logical login

---
+++ src/scripts/login.js
@@ -0,0 +1,41 @@
+
+const username = document.querySelector('#username-login');
+const password = document.querySelector('#password-login');
+const checkbox = document.querySelector('#checkbox-login');
+const button = document.querySelector('#button-login');
+
+const myUsername = 'alex';
+const myPassword = '123';
+
+button.addEventListener('', (event) => {
+  event.preventDefault();
+  
+  const usernameValue = username.value;
+  const passwordValue = password.value;
+  
+  if (usernameValue === '' || passwordValue === '') {
+    alert('Fill in all fields!');
+    return;
+  }
+  
+  if (usernameValue !== myUsername || passwordValue !== myPassword) {
+    alert('Incorrect username or password.');
+    return;
+  }
+
+  alert('Access released!');
+
+});
+
+checkbox.addEventListener('change', () => {
+  
+  const type = password.getAttribute('type');
+  
+  if (type === 'password') {
+    password.setAttribute('type', 'text');
+    return;
+  }
+  
+  password.setAttribute('type', 'password');
+
+});
